992b5950baa90fed6bfc3267550cf5b4b1ccd9b5
PRASANNA S <psp444333@gmail.com>
2026-09-08 10:25:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -1,48 +1,41 @@
 class Solution {
   public:
-  
-  stack<int> st;
-  void dfs(int i,vector<vector<int>>&adj,vector<int>&vis)
-  {
-      vis[i]=1;
-      
-      for(auto nei : adj[i])
-      {
-          if(!vis[nei])
-          {
-              dfs(nei,adj,vis);
-          }
-      }
-      st.push(i);
-  }
-  
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
-        vector<int> vis(V,0);
-        //cout<<true;
-        vector<vector<int>> adj(V);
-        vector<int> res;
-        for(int i=0;i<edges.size();i++)
-        {
-            int u=edges[i][0];
-            int v=edges[i][1];
-            adj[u].push_back(v);
-        }
+     // vector<int> vis(V,0);
+      vector<vector<int>> adj(V);
+      vector<int> res;
+      vector<int> indeg(V);
+      for(int i=0;i<edges.size();i++)
+      {
+          int u=edges[i][0];
+          int v=edges[i][1];
+          adj[u].push_back(v);
+          indeg[v]++;
+      }
+      
+      queue<int> q;
+      
+      for(int i=0;i<V;i++)
+      {
+          if(indeg[i]==0) q.push(i);
+      }
+      
+      while(!q.empty())
+      {
+          
+          int node=q.front();
+          q.pop();
+          res.push_back(node);
+          
+          for(auto nei :  adj[node])
+          {
+              indeg[nei]--;
+              
+              if(indeg[nei]==0) q.push(nei);
+          }
+          
+      }
         
-        for(int i=0;i<V;i++)
-        {
-            if(!vis[i])
-            {
-                dfs(i,adj,vis);
-            }
-        }
-        
-        while(!st.empty())
-        {
-            res.push_back(st.top());
-            st.pop();
-            
-        }
-        return res;
-        
+      return res;
     }
 };

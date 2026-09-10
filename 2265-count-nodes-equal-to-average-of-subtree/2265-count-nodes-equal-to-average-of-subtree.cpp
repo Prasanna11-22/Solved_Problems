b1b6a82ee6b8d9d794e5c51591af417ca8b44e8f
PRASANNA S <psp444333@gmail.com>
2026-09-10 10:42:45 +0530

    2265-count-nodes-equal-to-average-of-subtree 7 ms (54.96%) 15.8 MB (64.72%) Medium Tree

---
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int ans=0;
+
+pair<int,int> dfs(TreeNode* root)
+{
+    if(!root) return {0,0};
+    auto left=dfs(root->left);
+    auto right=dfs(root->right);
+
+    //cout<<root->val<<" "<<left.first<<" "<<right.first;
+
+    int val=left.first+root->val+right.first;
+    int llen=left.second;
+    int rlen=right.second;
+    int avg=(val/(llen+rlen+1));
+
+    if(root->val==avg) ans++;
+
+
+    return {val,llen + rlen + 1};
+}
+    int averageOfSubtree(TreeNode* root) {
+        dfs(root);
+        return ans;
+        
+    }
+};
